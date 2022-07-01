18324cf4b4d3263d8cc3ccffcad177e28b124971
ggichuru <ggichuru.dev@gmail.com>
2022-07-02 02:37:13 +0300

    [structs] Add tweet structs

--- programs/solanaTweet/src/lib.rs
+++ programs/solanaTweet/src/lib.rs
@@ -5,11 +5,43 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_tweet {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
 }
 
+/** --------------------------------------------------
+*                      STRUCTS
+----------------------------------------------------*/
+
+// Tweet Struct
+#[account] // An attribute for a data structure representing a solana account
+#[derive(Default)]
+pub struct Tweet {
+    message: String,
+    likes: u8,
+    creator: Pubkey,
+    people_who_liked: Vec<Pubkey>,
+}
+
+// TweetPlatform Struct
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TweetPlatform<'info> {
+    #[account(init, payer = user, space = 900)]
+    pub tweet: Account<'info, Tweet>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Write Tweet Struct
+#[derive(Accounts)]
+pub struct WriteTweet<'info> {
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
+}
+
+// Like Tweet Struct
+#[derive(Accounts)]
+pub struct LikeTweet<'info> {
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
+}
