cf675d66bc92a40981aa6699ee78ab853b62df2b
ggichuru <ggichuru.dev@gmail.com>
2022-07-07 23:02:26 +0300

    Solana app ready for deployment

--- programs/solanaTweet/src/lib.rs
+++ programs/solanaTweet/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GrZ1doFS5hagfoY5EGaggCVJBzAG8vGscq1jiV7bmUiU");
 
 #[program]
 pub mod solana_tweet {
