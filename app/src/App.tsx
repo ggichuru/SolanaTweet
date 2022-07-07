050870f5050a86ce6c5ac6220b16411e1e7a314d
ggichuru <ggichuru.dev@gmail.com>
2022-07-08 00:28:13 +0300

    connect to phantom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Connect2Phantom from "./components/connect2Phantom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Solana Tweet</h1>
+        <hr className="fullWidth" />
+
+        <p>Hello There</p>
+        <Connect2Phantom />
       </header>
     </div>
   );
---
+++ app/src/components/connect2Phantom.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState, FC } from "react";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+
+interface ConnectOpts {
+  onlyIfTrusted: boolean;
+}
+
+interface PhantomProvider {
+  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+  disconnect: () => Promise<void>;
+  on: (event: PhantomEvent, callback: (args: any) => void) => void;
+  isPhantom: boolean;
+}
+
+type WindowWithSolana = Window & {
+  solana?: PhantomProvider;
+};
+
+const Connect2Phantom: FC = () => {
+  const [walletAvail, setWalletAvail] = useState(false);
+  const [provider, setProvider] = useState<PhantomProvider | null>(null);
+  const [connected, setConnected] = useState(false);
+  const [pubKey, setPubKey] = useState<PublicKey | null>(null);
+
+  useEffect(() => {
+    if ("solana" in window) {
+      const solWindow = window as WindowWithSolana;
+
+      if (solWindow?.solana?.isPhantom) {
+        setProvider(solWindow.solana);
+        setWalletAvail(true);
+
+        // Attempt to connect to the wallet
+        solWindow.solana.connect({ onlyIfTrusted: true });
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    provider?.on("connect", (publicKey: PublicKey) => {
+      console.log(`connect event: ${publicKey}`);
+
+      setConnected(true);
+      setPubKey(publicKey);
+    });
+
+    provider?.on("disconnect", () => {
+      console.log("disconnect event");
+
+      setConnected(false);
+      setPubKey(null);
+    });
+  }, [provider]);
+
+  const connectHandler: React.MouseEventHandler<HTMLButtonElement> = (
+    event
+  ) => {
+    console.log(`connect handler`);
+
+    provider?.connect().catch((err) => {
+      console.error("connect error:", err);
+    });
+  };
+
+  const disconnectHandler: React.MouseEventHandler<HTMLButtonElement> = (
+    event
+  ) => {
+    console.log(`disconnect handler`);
+
+    provider?.disconnect().catch((err) => {
+      console.error("disconnect error:", err);
+    });
+  };
+
+  return (
+    <div>
+      {walletAvail ? (
+        <>
+          <button disabled={connected} onClick={connectHandler}>
+            Connect To Phantom
+          </button>
+          <button disabled={!connected} onClick={disconnectHandler}>
+            Disconnect from Phantom
+          </button>
+          {connected ? <p>Your public key is : {pubKey?.toBase58()}</p> : null}
+        </>
+      ) : (
+        <>
+        <p>Opps!!! Phantom is not available. Go get it <a href="https://phantom.app/">https://phantom.app/</a>.</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Connect2Phantom;
